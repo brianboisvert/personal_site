dfbb23c4abec54c1589d880e70faac16b41d3b17
brianboisvert <b.m.boisvert@gmail.com>
2018-11-28 21:58:47 -0500

    adding JS for sticky nav

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,10 +2,29 @@
 import React from 'react';
 import headshot from '../images/headshot.jpg';
 
-const LandingPage = (props) => {
+class LandingPage extends React.Component {
+
+  myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
+render() {
+
+  window.onscroll = function() {myFunction()};
+
+  // Get the navbar
+  var navbar = document.getElementById("navbar");
+
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 
   return(
-    // LANDING PAGE
     <div>
       <div className="landing-page">
         <div className="landing-page-text">
@@ -17,7 +36,6 @@ const LandingPage = (props) => {
           </div>
         </div>
       </div>
-
 
       <div>
         <div className="about">
@@ -32,8 +50,15 @@ const LandingPage = (props) => {
           </div>
         </div>
       </div>
+
+      <div>
+
+      </div>
+
+
     </div>
   )
+}
 }
 
 export default LandingPage;
