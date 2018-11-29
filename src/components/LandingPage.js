e5cb547a895728fb7cc33361284887ef88a0005f
brianboisvert <b.m.boisvert@gmail.com>
2018-11-28 19:29:59 -0500

    typing functionality

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -21,11 +21,13 @@ const LandingPage = (props) => {
 
       <div>
         <div className="about">
-          <div className="headshot-container">
             <img className="headshot" src={headshot} alt="portrait" />
-          </div>
           <div className="about-text">
-            I’ve done a lot of things in my short professional career. <br/><br/>After graduating from the University of Massachusetts Amherst in 2014, I moved to New York and worked in the crazy, yet rewarding, world of sales at Yelp. I then followed a dream of working in the fashion industry to Tory Burch where I was deeply involved in both the design and sales processes as a women’s footwear merchandiser. I then indulged my wanderlust and taught English in elementary schools in the South of France.<br/><br/> It’s a pretty diverse background, sure, but what drove me has never changed. <strong>I love creating. I love learning. I love a good challenge.</strong> So here I am now, a recent graduate of the Flatiron School immersive web development program and a professional computer programmer.<br /><br />-Brian
+            <div className="typer">
+              <h1>Hey there, I'm Brian.</h1>
+              <p>And I'm a full-stack developer.</p>
+
+            </div>
           </div>
         </div>
       </div>
